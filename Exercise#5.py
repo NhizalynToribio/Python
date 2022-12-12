2153613a6d20749242c3b8159733a8fbe38539b3
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 21:50:50 +0800

    start of exercise#5

---
+++ Exercise#5.py
@@ -0,0 +1,4 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
