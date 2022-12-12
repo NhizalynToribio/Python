dbddeb6d21a2df908b7c00e0973718a4fff6c005
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 20:46:49 +0800

    def statement and __init__ statement is stated below the class statement

--- Exercise#3.py
+++ Exercise#3.py
@@ -3,7 +3,11 @@ print("************** NHIZALYN TORIBIO ****************")
 print("*************** BSCOE 2 - 2 *******************")
 print("**Python Stacks, Queues, and Priority Queues **")
 
+
 from collections import deque
 
 
 class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
