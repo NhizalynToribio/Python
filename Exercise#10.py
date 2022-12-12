db97be630c921e4c01fdd2ef7ab6da730220e04e
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 23:17:55 +0800

    start of exercise#10, with person1 to person3 contaning data of a person

---
+++ Exercise#10.py
@@ -0,0 +1,8 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
