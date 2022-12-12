d1812932c7ee89defbdd1bba1d6b5c4b1eb8af98
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 22:33:20 +0800

    lifo with append from 1-3

--- Exercise#7.py
+++ Exercise#7.py
@@ -3,4 +3,10 @@ print("************** NHIZALYN TORIBIO ****************")
 print("*************** BSCOE 2 - 2 *******************")
 print("**Python Stacks, Queues, and Priority Queues **")
 
+
 lifo = []
+
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
