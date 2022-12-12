65151751e86e2bb6a810cb017bff99583d8b48ef
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 20:43:04 +0800

    start of exercise#3, adding from and import statement

---
+++ Exercise#3.py
@@ -0,0 +1,6 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+
+from collections import deque
