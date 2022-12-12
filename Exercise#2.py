c4562cf0a8eaa80d5652543ca8887c567561b1fa
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 19:20:50 +0800

    st, adding names and making from statement and queue and also import statement

---
+++ Exercise#2.py
@@ -0,0 +1,7 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+
+
+from queues import Queue
