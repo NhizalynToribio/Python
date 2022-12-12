51ec94153d3f470482df52842cde9a28e5123174
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 11:14:09 +0800

    Initial commit

---
+++ Exercise#1.py
@@ -0,0 +1,8 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+
+# queues.py
+
+from collections import deque
