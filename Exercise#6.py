dcc832af5399c6a602a7ca59f4ecfb0d9ee788de
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 22:12:02 +0800

    from statement and import statement

--- Exercise#6.py
+++ Exercise#6.py
@@ -3,3 +3,4 @@ print("************** NHIZALYN TORIBIO ****************")
 print("*************** BSCOE 2 - 2 *******************")
 print("**Python Stacks, Queues, and Priority Queues **")
 
+from queues import Stack
