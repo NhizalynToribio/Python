0b47e69597be96f9b8ccc10a74b3da5f2ea2b1c0
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-12 19:35:04 +0800

    making fifo dequeue statement

--- Exercise#2.py
+++ Exercise#2.py
@@ -6,9 +6,16 @@ print("**Python Stacks, Queues, and Priority Queues **")
 
 from queues import Queue
 
-
+# fifo enqueue statement
 fifo = Queue()
 fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
+# fifo dequeue statement
+fifo.dequeue()
+'1st'
+fifo.dequeue()
+'2nd'
+fifo.dequeue()
+'3rd'
