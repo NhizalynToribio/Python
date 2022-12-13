709a755f5d86151e19651a6537aa366cbdbc2256
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-13 18:27:00 +0800

    done making of def dequeue with return syntax

--- Exercise#1 - queues.py
+++ Exercise#1 - queues.py
@@ -174,3 +174,9 @@ messages.dequeue()
 class PriorityQueue:
     def __init__(self):
         self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
