5555b748d098e748efa19104417fdca27e4631e8
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-13 20:05:59 +0800

    done with wiperz and hazard_lights with message

--- Exercise#1 - queues.py
+++ Exercise#1 - queues.py
@@ -201,3 +201,7 @@ from dataclasses import dataclass
 @dataclass
 class Message:
     event: str
+
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
