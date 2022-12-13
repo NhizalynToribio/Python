3ceaef64ba438458a206cc9cf1ca9335ee4cdb23
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-13 14:23:08 +0800

    done making def statement with __len__ and with return code

--- Exercise#1 - queues.py
+++ Exercise#1 - queues.py
@@ -44,3 +44,6 @@ from collections import deque
 class Queue:
     def __init__(self, *elements):
         self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
