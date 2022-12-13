23894eb5f25621c9955614fcf18601383a28a5ae
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-13 20:47:34 +0800

    def__iter__ with while loop and yield is stated in

--- Exercise#1 - queues.py
+++ Exercise#1 - queues.py
@@ -246,3 +246,10 @@ class IterableMixin:
     def __len__(self):
         return len(self._elements)
 
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+
+
+
