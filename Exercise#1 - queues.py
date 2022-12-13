4e8db5d2028357f4bd1d7b8b002c792d881b990c
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-13 20:27:15 +0800

    done making class PriorityQueue with def__init__self are stated.

--- Exercise#1 - queues.py
+++ Exercise#1 - queues.py
@@ -225,3 +225,8 @@ messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
 from collections import deque
 from heapq import heappop, heappush
 from itertools import count
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
