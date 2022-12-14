77aaf42269fb629e8814b17b46e82cc918a0db33
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-14 11:46:09 +0800

    making some changes

--- Exercise#4.py
+++ Exercise#4.py
@@ -3,18 +3,14 @@ print("************** NHIZALYN TORIBIO ****************")
 print("*************** BSCOE 2 - 2 *******************")
 print("**Python Stacks, Queues, and Priority Queues **")
 
+
 from queues import Queue
 
 fifo = Queue("1st", "2nd", "3rd")
 len(fifo)
-3
+
 
 for element in fifo:
     print(element)
-...
-1st
-2nd
-3rd
 
 len(fifo)
-0
