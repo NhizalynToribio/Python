ec17f8a1403b2f4f7b1fc4feae77adaac63aa290
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-14 22:41:48 +0800

    Start FIFO & LIFO thread safe queue and import statements

---
+++ FIFOandLIFO_ThreadSafeQueues.py
@@ -0,0 +1,26 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+print("**** Thread Safe Queues - FIFO & LIFO *****")
+
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+import threading
+
+from random import randint
+from time import sleep
+
+from random import choice, randint
+
+from itertools import zip_longest
+
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
+
+
+
+
