9233823e07bcf84f62b7047e3ff39f315a35dbb8
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-14 19:19:42 +0800

    start of Threading Safe Queues - Priority Queue

---
+++ PriorityQueue_ThreadSafeQueues.py
@@ -0,0 +1,10 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+print("**** Thread Safe Queues - Priority Queue *****")
+
+# importing argparse, LifoQueue, PriorityQueue, Queue and threading
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+import threading
