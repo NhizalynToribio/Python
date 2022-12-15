b4a6f3469a1f7a0b8bc551938c8fbad85718afb0
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-15 17:48:00 +0800

    Start of Queues in Map, from and import statements with class attribute

---
+++ Map/queues.py
@@ -0,0 +1,49 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+print("*************** Roadmap - Queues *******************")
+
+from collections import deque
+from dataclasses import dataclass
+from heapq import heapify, heappop, heappush
+from itertools import count
+from typing import Any
+
+
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
