f6cf4fcdea378ac23036a67863a1509d0d39b97d
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-15 17:44:40 +0800

    few changes

--- Map/Graph.py
+++ Map/Graph.py
@@ -2,7 +2,7 @@ print("**************** PROGRAMMED BY: *****************")
 print("************** NHIZALYN TORIBIO ****************")
 print("*************** BSCOE 2 - 2 *******************")
 print("**Python Stacks, Queues, and Priority Queues **")
-print("*************** Graph *******************")
+print("*************** Roadmap - Graph *******************")
 
 # Necessary modues
 from typing import NamedTuple
