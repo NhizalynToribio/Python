c65609db3fcfe1159aeef8412d88ee2521b2f8a6
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-16 01:54:39 +0800

    print showing the roadmap.dot

--- Map/dotread.py
+++ Map/dotread.py
@@ -5,4 +5,9 @@ print("**Python Stacks, Queues, and Priority Queues **")
 print("*************** Dot Reading Files *******************")
 
 import networkx as nx
-from graph import *
+from Graph import *
+
+print(nx.nx_agraph.read_dot("roadmap.dot"))
+
+Graph = nx.nx_agraph.read_dot("roadmap.dot")
+print(Graph.nodes["london"])
