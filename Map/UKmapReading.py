6912b2f124c26f274e94bd81e79f2332258f475d
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-15 20:55:54 +0800

    6th testing to 9th testing

--- Map/UKmapReading.py
+++ Map/UKmapReading.py
@@ -66,3 +66,62 @@ for node in nx.bfs_tree(graph, nodes["edinburgh"]):
         break
 else:
     print("Not found")
+
+
+print("\n6th testing")
+
+
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
+
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
+
+# Testing the breadth-first search and traversal implementations
+print("\n7th testing")
+
+
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
+print(city.name)
+print("\n")
+for city in breadth_first_traverse(graph, nodes["edinburgh"]):
+    print(city.name)
+
+# Shortest Path Using Breadth-First Traversal
+# Revealing the shortest path between two cities
+print("\n8th testing: Using networkx to reveal all the shortest paths between two cities")
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
+
+# Queue-based implementation of the shortest path
+# When you call the queue-based implementation of the shortest path, you get the same results as with networkx
+print("\n9th testing: Queue-based implementation of the shortest path")
+print(" → ".join(city.name for city in shortest_path(graph, city1, city2)))
+
+
+def by_latitude(city):
+    return -city.latitude
+
+
+print(" → ".join(
+    city.name
+    for city in shortest_path(graph, city1, city2, by_latitude)
+))
