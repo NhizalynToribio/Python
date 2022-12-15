5bc3565326bfc8ba1b1ba9fad3efec268b3e5012
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-16 02:35:41 +0800

    Apache Kafka - producer file

---
+++ Message Queues/Apache Kafka/producer.py
@@ -0,0 +1,16 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+print("** Distributed Message Queue - Apache Kafka ***")
+
+
+from kafka3 import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+while True:
+    message = input("Message: ")
+    producer.send(
+        topic="datascience",
+        value=message.encode("utf-8"),
+    )
