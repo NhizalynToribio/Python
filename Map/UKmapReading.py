34592458d8020769fa085e43718bc071ecb653ee
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-15 20:41:05 +0800

    testing 2 to 5th testing of the map

--- Map/UKmapReading.py
+++ Map/UKmapReading.py
@@ -25,3 +25,44 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print(nodes["london"])
 print("\n")
 print(graph)
+
+
+print("\n2nd testing")
+for neighbor in graph.neighbors(nodes["london"]):
+    print(neighbor.name)
+
+
+print("\n3rd testing")
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
+
+
+print("\n4th testing")
+
+
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
+
+print("\n5th testing")
+
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
