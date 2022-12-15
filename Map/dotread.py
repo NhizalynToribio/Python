05dc77fabc456515fe3502724aed9d45e6b47b2f
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-15 19:07:52 +0800

    start, import networkx as nx and from import syntax

---
+++ Map/dotread.py
@@ -0,0 +1,8 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+print("*************** Dot Reading Files *******************")
+
+import networkx as nx
+from graph import *
