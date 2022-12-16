332aba8b983d314b0036127e2d47c37d7683e755
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-12-16 16:47:02 +0800

    start of multi processing queue, and class Combination statement

---
+++ multiprocessingqueue.py
@@ -0,0 +1,37 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("**Python Stacks, Queues, and Priority Queues **")
+print("**** Multi Processing Queue *****")
+
+import time
+from hashlib import md5
+from string import ascii_lowercase
+import multiprocessing
+from dataclasses import dataclass
+import argparse
+import queue
+import time
+
+
+POISON_PILL = None
+
+
+class Combinations:
+    def __init__(self, alphabet, length):
+        self.alphabet = alphabet
+        self.length = length
+
+    def __len__(self):
+        return len(self.alphabet) ** self.length
+
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError
+        return "".join(
+            self.alphabet[
+                (index // len(self.alphabet) ** i) % len(self.alphabet)
+            ]
+            for i in reversed(range(self.length))
+    )
+
